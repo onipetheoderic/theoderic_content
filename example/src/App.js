314498f0ee5bc3d7734a4985cb0b49497a538467
theoderic <onipetheoderic@gmail.com>
2019-08-07 03:04:59 +0100

    built google text content to make the code dry

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,19 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent text='Modern React component module' 
+        size={20} 
+        marginTop = {20}
+    marginBottom= {10}
+    marginLeft= {10}
+    marginRight= {10}
+    paddingLeft= {10}
+    paddingRight= {10}
+    paddingTop= {10}
+    paddingBottom = {5}
+    fontStyle="italic"
+    smallCaps={true}
+    googleFont="Baumans"/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
+import WebFont from 'webfontloader';
+import allFonts from './allfont'
+/**/
+WebFont.load({
+  google: {
+    families: allFonts,
+  }
+});
 
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    size: PropTypes.number,
+    marginTop: PropTypes.number,
+    marginBottom: PropTypes.number,
+    marginLeft: PropTypes.number,
+    marginRight: PropTypes.number,
+    paddingLeft: PropTypes.number,
+    paddingRight: PropTypes.number,
+    paddingTop: PropTypes.number,
+    paddingBottom: PropTypes.number,
+    fontStyle: PropTypes.string,
+    smallCaps: PropTypes.bool,
+    googleFont: PropTypes.string
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    const {text} = this.props
+    console.log(this.props.size)
+    let caps = this.props.smallCaps;
+    caps = true ? "small-caps" : "normal";
+
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div style={{marginTop: this.props.marginTop, marginRight: this.props.marginRight, marginBottom: this.props.marginBottom, marginLeft: this.props.marginRight}}>
+        <p style={{fontFamily:this.props.googleFont, fontVariant: caps, fontStyle: this.props.fontStyle, fontSize: this.props.size, paddingBottom: this.props.paddingBottom, paddingLeft: this.props.paddingLeft, paddingRight: this.props.paddingRight, paddingTop: this.props.paddingTop}}>
+        {this.props.text}</p>
+
       </div>
     )
   }
